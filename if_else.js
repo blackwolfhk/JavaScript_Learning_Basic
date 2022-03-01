aaeb297017fda29d40b8cdb8d7ac7178875cf3fc
KC <76124063+blackwolfhk@users.noreply.github.com>
2022-03-01 23:34:38 +0800

    Create if_else.js

---
+++ if_else.js
@@ -0,0 +1,11 @@
+// A very simple if...else example
+
+const marketCap = 123456789 // <- to input any amount of marketdap here
+
+if (marketCap >= 500000000) {
+    console.log("The company might be qualified to list on Main Board~")
+} else if (marketCap >= 45000000 && marketCap < 500000000) {
+    console.log("The company might be qualified to list on GEM~")
+} else {
+    console.log("The company might give up to list~")
+}
