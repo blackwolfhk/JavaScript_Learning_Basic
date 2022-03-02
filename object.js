93004d51191a81960eb59f089e7c6d3a1028f6f0
KC <76124063+blackwolfhk@users.noreply.github.com>
2022-03-02 23:04:26 +0800

    Update object.js

--- object.js
+++ object.js
@@ -52,3 +52,59 @@ console.log(checkDuplicate)
 // { a: 2, b: 1, c: 1, d: 1, e: 1 }
 // { a: 2, b: 1, c: 1, d: 2, e: 1 }
 // { a: 2, b: 1, c: 1, d: 2, e: 1 }
+
+
+
+
+
+
+// Example
+// Please use JavaScript's data types (Object, Array, String, Number, etc) to construct a variable holding below data:
+
+// Name	   Height	  Hobbies
+// Peter	175	      Football, Basketball
+// John	    180	      Basketball, Sleeping
+// Betty    170	      Reading
+// Mary	    160	      Eating, Sleeping, Reading
+
+// Then, using while loop to show the names of people who are taller than 170.
+
+const friends = []
+
+friends.push({
+    "name": "Peter",
+    "height": 175,
+    "Hobbies": ["Football", "Basketball"]
+})
+
+friends.push({
+    "name": "John",
+    "height": 180,
+    "Hobbies": ["Basketball", "Sleeping"]
+})
+
+friends.push({
+    "name": "Betty",
+    "height": 170,
+    "Hobbies": ["Reading"]
+})
+
+friends.push({
+    "name": "Mary",
+    "height": 160,
+    "Hobbies": ["Eating", "Sleeping", "Reading"]
+})
+
+console.log(friends)
+
+let x = 0
+while (x < friends.length) {
+    if(friends[x]["height"]>170) {
+        console.log(friends[x]["name"])
+    }
+    x++
+}
+
+
+
+
